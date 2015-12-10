e44b772cd95b1e206684b93c20ec618623cd432f
tuxan <3dmann@gmail.com>
2015-12-10 13:12:49 -0300

    Problem with onLoadFinished and onCreateOptionsMenu coordination solved.

--- alexandria/app/src/main/java/it/jaschke/alexandria/BookDetailFragment.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/BookDetailFragment.java
@@ -66,6 +66,19 @@ public class BookDetailFragment extends FragmentBase implements LoaderManager.Lo
 
         MenuItem menuItem = menu.findItem(R.id.action_share);
         shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // If onLoadFinished happens before this, we can go ahead and set the share intent now.
+        if (bookTitle != null)
+            shareActionProvider.setShareIntent(createShareIntent());
+
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
+        return shareIntent;
     }
 
     @Override
@@ -105,13 +118,11 @@ public class BookDetailFragment extends FragmentBase implements LoaderManager.Lo
         }
 
         bookTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
-            ((TextView) rootView.findViewById(R.id.fullBookTitle)).setText(bookTitle);
+        ((TextView) rootView.findViewById(R.id.fullBookTitle)).setText(bookTitle);
 
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-        shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
-        shareActionProvider.setShareIntent(shareIntent);
+        // If onCreateOptionsMenu happens before this, we can go ahead and set the share intent now.
+        if (shareActionProvider != null)
+            shareActionProvider.setShareIntent(createShareIntent());
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         if (bookSubTitle != null) {
